90f532f4e3a960a2c47d78f079a291a3282bf01d
Muhammad Sulaiman <sulaimanperson@gmail.com>
2022-01-07 00:39:14 +0500

    all set and done

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,9 @@ function App() {
   const [showPanel, setshowPanel] = useState("-translate-x-full");
   const [opacityLink, setopacityLink] = useState("opacity-70");
   const [overflowToggle, setoverflowToggle] = useState("overflow-hidden");
-  const [iconToggle, seticonToggle] = useState("far fa-bars text-2xl")
+  const [iconToggle, seticonToggle] = useState("far fa-bars text-2xl");
+  const [showRightBar, setshowRightBar] = useState("-translate-x-full");
+
 
 
   let showPanelFunction = () =>{
@@ -40,11 +42,23 @@ function App() {
       setoverflowToggle('overflow-hidden')
   
       }
+
+      if (window.innerWidth < 640) {
+        setshowRightBar('-transform-x-0');
+    }
   }
 
+let closeSideBarMobilRight = () =>{
+  setshowRightBar('-translate-x-full') 
+  setopacityLink('opacity-70')
+}
 
 
+let closeAllSideBars = () =>{
+  seticonToggle('far fa-bars text-2xl');
+  setshowPanel('-translate-x-full');
 
+}
   return (
   <>
 <div className="w-full h-16 bg-[#262626] text-white flex items-center justify-between px-6">
@@ -67,11 +81,13 @@ function App() {
 
 
 
-<div className={`absolute  top-16 left-0 py-5 w-[250px] bg-[#262626] text-white pl-6 pr-6 transition transform ${showPanel} ${overflowToggle}`}>
+<div className={`absolute  top-0 sm:top-16 z-50 left-0 w-screen h-screen sm:h-auto  py-5 pt-20 sm:pt-5 sm:w-[250px] bg-[#262626] text-white pl-6 pr-6 transition transform ${showPanel} ${overflowToggle}`}>
+<i className="far fa-times sm:hidden text-2xl text-white absolute top-5 left-5 cursor-pointer" onClick={closeAllSideBars}></i>
+
 <div className="flex gap-5 flex-col">
 <a href="#" className="opacity-70 hover:opacity-100">HOME PAGE</a>
 <a href="#" className="opacity-70 hover:opacity-100">DIRETTE</a>
-<a href="#" onClick={functionOpacity} className={`flex items-center hover:opacity-100 justify-between opacity-70 ${opacityLink}`}>ON DEMAND <i className="fal fa-angle-right text-2xl"></i></a>
+<a href="#" onClick={functionOpacity} className={`flex items-center hover:opacity-100 gap-5 w-fit sm:w-auto justify-between opacity-70 ${opacityLink}`}>ON DEMAND <i className="fal fa-angle-right text-2xl"></i></a>
 <a href="#" className="opacity-70 hover:opacity-100">ABBONAMENTI</a>
 <a href="#" className="opacity-70 hover:opacity-100">SCUOLE</a>
 <a href="#" className="flex w-fit opacity-70 hover:opacity-100 gap-5 items-center justify-between">ITA <i className="fal fa-angle-down text-2xl"></i></a>
@@ -80,16 +96,16 @@ function App() {
 <a href="#" className="opacity-70 hover:opacity-100">Vai al sito ufficiale</a>
 </div>
 
-<div className={`absolute flex gap-5 flex-col right-[-89%] top-0 bg-[#0f0f0f] px-14 py-6  h-full`}>
-
-<a href="#" className="opacity-70 hover:opacity-100">OPERA</a>
-<a href="#" className="opacity-70 hover:opacity-100">BALLETTO</a>
-<a href="#" className="opacity-70 hover:opacity-100">CONCERTI</a>
-<a href="#" className="opacity-70 hover:opacity-100">BAMBINI</a>
-<a href="#" className="opacity-70 hover:opacity-100">PLAYLIST</a>
-<a href="#" className="opacity-70 hover:opacity-100">DOCUMENTARI</a>
-<a href="#" className="opacity-70 hover:opacity-100">BALLETTO</a>
-<a href="#" className="opacity-70 hover:opacity-100">INTERVISTE</a>
+<div className={`absolute flex gap-5 flex-col right-0 sm:right-[-89%] top-0 bg-[#0f0f0f] px-6 sm:px-14 py-2 pt-10 h-full w-full sm:w-auto transition transform sm:-translate-x-0 ${showRightBar}`}>
+<i className="fal fa-angle-left sm:hidden cursor-pointer text-4xl opacity-80" onClick={closeSideBarMobilRight}></i>
+<a href="#" className="opacity-100">OPERA</a>
+<a href="#" className="opacity-100">BALLETTO</a>
+<a href="#" className="opacity-100">CONCERTI</a>
+<a href="#" className="opacity-100">BAMBINI</a>
+<a href="#" className="opacity-100">PLAYLIST</a>
+<a href="#" className="opacity-100">DOCUMENTARI</a>
+<a href="#" className="opacity-100">BALLETTO</a>
+<a href="#" className="opacity-100">INTERVISTE</a>
 
 
 
